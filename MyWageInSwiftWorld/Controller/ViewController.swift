4ecf65bd576b24751ff78aaa6bbd41d3facd1fb6
Gorloff <gorloff.slava@gmail.com>
2017-09-28 22:28:53 +0600

    2/4/4

--- MyWageInSwiftWorld/Controller/ViewController.swift
+++ MyWageInSwiftWorld/Controller/ViewController.swift
@@ -17,7 +17,17 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let calculateButton = UIButton(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 60))
+        calculateButton.backgroundColor = #colorLiteral(red: 0.9450980392, green: 0.3176470588, blue: 0.2235294118, alpha: 1)
+        calculateButton.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calculateButton.setTitle("Вычислить", for: .normal)
+        calculateButton.addTarget(self, action: #selector(calculate), for: .touchUpInside)
+        
+        self.wageTextField.inputAccessoryView = calculateButton
+        self.itemPriceTextField.inputAccessoryView = calculateButton
+        
+        self.resultLabel.isHidden = true
+        self.hoursLabel.isHidden = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,6 +35,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func clearButtonPressed(_ sender: Any) {
+        self.resultLabel.isHidden = true
+        self.hoursLabel.isHidden = true
+        self.wageTextField.text = ""
+        self.itemPriceTextField.text = ""
+    }
+    
+    @IBAction func calculate() {
+        if let wageText = self.wageTextField.text, let priceText = self.itemPriceTextField.text {
+            if let wage = Double(wageText), let price = Double(priceText) {
+                self.view.endEditing(true)
+                self.resultLabel.isHidden = false
+                self.hoursLabel.isHidden = false
+                self.resultLabel.text = "\(Wage.hours(forWage: wage, withPrice: price))"
+            }
+        }
+    }
+    
 }
 
